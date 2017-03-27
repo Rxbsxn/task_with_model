16b4289ce4c04bc5253432c39e37112bf1183b00
Robert Harężlak <harezlak.robert7@gmail.com>
2017-03-28 00:43:38 +0200

    mock of users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Job.create(name: FFaker::Food.fruit, start_date: '02.01.2017', end_date: '04.01.2017')
+User.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.email)
+
+user1 = User.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.email)
+Profile.create(user_id: user1, phone_number: FFaker::PhoneNumber.short_phone_number, accepted_at: nil)
+
+
+user2 = User.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.email)
+Profile.create(user_id: user2, phone_number: FFaker::PhoneNumber.short_phone_number, accepted_at: Date.today)
+Availability.create(user_id: user2, day_off: nil)
+
+user3 = User.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.email)
+Profile.create(user_id: user3, phone_number: FFaker::PhoneNumber.short_phone_number, accepted_at: Date.today)
+Availability.create(user_id: user3, day_off: ['01.01.2017', '05.01.2017'])
+
+user4 = User.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.email)
+Profile.create(user_id: user4, phone_number: FFaker::PhoneNumber.short_phone_number, accepted_at: Date.today)
+Availability.create(user_id: user4, day_off: ['02.01.2017', '10.01.2017'])
+
